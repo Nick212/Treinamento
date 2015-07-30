e18c6077df2a6706594e6626cc9240a2ea4accdf
Nick212 <nick_carvalho29@hotmail.com>
2015-07-30 20:28:10 -0300

    implementando comentario

--- Treinamento/NodeJS/aula1.js
+++ Treinamento/NodeJS/aula1.js
@@ -6,10 +6,11 @@ var server = http.createServer(function(request, response){
 	response.end();
 });
 
-
+//1-> Uma Forma de Subir o  Arquivo ao servidor
 //server.listen(3000);
 //localhost:3000
 
+//2-> Outra Forma de Subir o  Arquivo ao servidor
 server.listen(3000, function(){
 	console.log("Servidor Hello World rodando!");
 });
