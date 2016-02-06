643efb648670801bbd306e75eab3c0fd2e08adca
Nick212 <nick_carvalho29@hotmail.com>
2016-02-06 21:52:59 -0200

    Novos projetos de language learning  adicionados para versionamento

---
+++ Treinamento/MVC_4/AplicacaoCodeFirst/AplicacaoCodeFirst/BlogContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AplicacaoCodeFirst
+{
+    /// <summary>
+    /// Classe de Contexto aplicada para utilizar um serviço específico
+    /// </summary>
+    public class BlogContext : DbContext
+    {
+        public BlogContext()
+            : base("name=BlogContext")
+        {
+            Database.Connection.ConnectionString = 
+            @"data source=NICOLAS-PC\SQLEXPRESS; inicial catalog=BlogBDLivro; Integrated Security=SSPI";
+        }
+
+        public DbSet<Categorias> Categorias { get; set; }
+
+        public DbSet<Posts> Posts { get; set; }
+    }
+}
