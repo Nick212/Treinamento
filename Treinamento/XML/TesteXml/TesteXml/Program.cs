76099291b782529f1b7bc4ebc3c6af002c38b22b
Nicolas <nicolas.carvalho@dotz.com.br>
2016-02-26 09:31:03 -0300

    Learning working XML

---
+++ Treinamento/XML/TesteXml/TesteXml/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace TesteXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var mensagem =
+                "<?xml version='1.0' encoding='UTF-8'?>\n<error xmlns='http://ideais-commerce.com.br/api/'><error_code>8001</error_code><error_message>O pedido 7212282 do parceiro sitenormalAcom já existe na B2W, o número do pedido na B2W é 02-622040805, gravado no dia 2016-02-24 11:23:38.948.</error_message></error>";
+
+            XDocument doc = XDocument.Parse(mensagem);
+            Console.WriteLine(doc);
+
+            var response = new ResponseFusion().ObterRespostaDeserializada(mensagem);
+
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ Treinamento/XML/TesteXml/TesteXml/ResponseFusion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TesteXml
+{
+    public class ResponseFusion
+    {
+        #region Atributos
+        private int errorCode;
+        private string errorMessage;
+        #endregion
+
+        #region Proprieties
+        public int ErrorCode
+        {
+            get { return errorCode; }
+            set { errorCode = value; }
+        }
+
+        public string ErrorMessage
+        {
+            get { return errorMessage; }
+            set { errorMessage = value; }
+        }
+        #endregion
+
+        public List<string> ObterRespostaDeserializada(string message)
+        {
+            List<string> errorCode = new List<string>();
+
+            TextReader sr = new StringReader(message);
+            XElement xml = XElement.Load(sr);
+
+            foreach (XElement x in xml.Elements())
+            {
+                errorCode.Add(x.Value);
+            }
+            return errorCode;
+        }
+
+
+    }
+
+}
